fd50b77e196b2ac08e85917061e89443468ec7b6
MrScopes <drewmanrules23@gmail.com>
2018-11-06 23:19:45 -0500

    Update message.js

--- events/message.js
+++ events/message.js
@@ -1,9 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
 module.exports = (client, message) => {
     if (message.author.bot) return;
-    if (message.content.indexOf(client.config.prefix) !== 0) return;
-    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+    const conf = client.settings.ensure(message.guild.id, defaultS);
+    const prefix = conf.prefix;
+    if (message.content.indexOf(prefix) !== 0) return;
+    const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     const cmd = client.commands.get(command);
     if (!cmd) return;
     cmd.run(client, message, args);
-  };
+};
