2cf5c2db76f5e26afeedc46ec674ecb3c724e192
MrScopes <drewmanrules23@gmail.com>
2018-11-07 01:00:55 -0500

    Create eval.js

---
+++ commands/eval.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+function evalClean(text) {
+	if (typeof(text) === 'string')
+		return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));
+	else
+		return text;
+    }
+    
+module.exports.run = async (client, message, args) => {
+if (message.author.id == '221659063312842752') {
+    try {
+        const code = args.join(' ');
+        let evaled = eval(code);
+        if (typeof evaled !== 'string')
+            evaled = require('util').inspect(evaled);
+            message.channel.send(evalClean(evaled), {code:'xl'});
+    } catch (err) {
+        embed = new Discord.RichEmbed()
+            .setDescription(`**Error:**\n ${evalClean(err)}`)
+            message.channel.send(embed);
+        }
+}
+};
