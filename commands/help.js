14cef8240e778ffba4aec81fbb8917bbb443c570
MrScopes <drewmanrules23@gmail.com>
2018-11-07 00:16:23 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,8 @@ module.exports.run = async (client, message, args) => {
     const conf = client.settings.ensure(message.guild.id, defaultS);
     const prefix = conf.prefix;
     message.channel.send([
-        `\`${prefix}help\`: \`Why hello there\``,
-        `\`${prefix}ping\`: \`View the bot's ping\``
+        `\`${prefix}help\`: \`list of commands\``,
+        `\`${prefix}ping\`: \`view the bot's ping\``,
+        `\`${prefix}uptime\`: \`view the bot's uptime\``
     ])
 }
