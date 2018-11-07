6a8c9536336a43fbb618324063851cfee70834a1
MrScopes <drewmanrules23@gmail.com>
2018-11-07 01:00:35 -0500

    Create info.js

---
+++ commands/info.js
@@ -0,0 +1,8 @@
+module.exports.run = async (client, message, args) => {
+    message.channel.send([
+        `**CREATOR:** \`MrScopes#5546\``,
+        `**GITHUB:** https://github.com/MrScopes/Escanor`,
+        `**FORUMS:** https://escanor.ga/`,
+        `**CREATED:** \`November 7th, 2018\``
+    ])
+};
