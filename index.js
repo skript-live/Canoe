3d464ae328c6f5c792e229f6002862fa9e71f372
MrScopes <drewmanrules23@gmail.com>
2018-11-06 20:45:25 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+const Discord = require('discord.js');
+const Enmap = require('enmap');
+const fs = require('fs');
+const client = new Discord.Client();
+const config = require('./config.json');
+client.commands = new Enmap();
+client.config = config;
+
+fs.readdir('./events/', (err, files) => {
+	if (err) return console.error(err);
+	files.forEach(file => {
+		const event = require(`./events/${file}`);
+		let eventName = file.split('.')[0];
+		console.log(`Attempting to load event '${eventName}'`)
+		client.on(eventName, event.bind(null, client));
+	});
+});
+
+fs.readdir('./commands/', (err, files) => {
+	if (err) return console.error(err);
+	files.forEach(file => {
+		if (!file.endsWith('.js')) return;
+		let props = require(`./commands/${file}`);
+		let commandName = file.split('.')[0];
+		console.log(`Attempting to load command '${commandName}'`);
+		client.commands.set(commandName, props);
+	});
+});
+
+client.on('ready', () => {
+	console.log(`Successfully logged in as ${client.user.tag}`);
+});
+
+client.on('error', console.error);
+client.on('promiseRejection', console.error);
+
+client.login(config.token);
