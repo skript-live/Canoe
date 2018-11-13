a9ad0a565958b9de0d3de745399fafc35858f136
MrScopes <drewmanrules23@gmail.com>
2018-11-13 08:57:21 -0500

    PULL

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,8 +1,5 @@
+const ms = require('ms');
+
 module.exports.run = (client, message, args) => {
-    totalSeconds = (client.uptime / 1000);
-    hours = Math.floor(totalSeconds / 3600);
-    totalSeconds %= 3600;
-    minutes = Math.floor(totalSeconds / 60);
-    seconds = totalSeconds % 60;
-    message.channel.send(`**UPTIME:** \`${hours} hours\`, \`${minutes} minutes\` and \`${Math.round(seconds)} seconds\``)
+    message.channel.send(`**UPTIME:** \`${ms(client.uptime}\)}\``)
 };
