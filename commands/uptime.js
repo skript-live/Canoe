0fbc1e4ff9146740243c1ed031c0f7f6e5fc4360
MrScopes <drewmanrules23@gmail.com>
2018-11-13 08:58:04 -0500

    PULL

--- commands/uptime.js
+++ commands/uptime.js
@@ -1,5 +1,5 @@
 const ms = require('ms');
 
 module.exports.run = (client, message, args) => {
-    message.channel.send(`**UPTIME:** \`${ms(client.uptime}\)}\``)
+    message.channel.send(`**UPTIME:** \`${ms(client.uptime)}\``)
 };
