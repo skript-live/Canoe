accc630e8f1205a11061b6af2059311e3107fc32
MrScopes <drewmanrules23@gmail.com>
2018-11-13 09:35:55 -0500

    PULL

--- events/message.js
+++ events/message.js
@@ -6,6 +6,14 @@ module.exports = (client, message) => {
     const conf = client.settings.ensure(message.guild.id, defaultS);
     const prefix = conf.prefix;
     if (message.content.indexOf(prefix) !== 0) return;
+    const cd = new Set();
+    if (cd.has(message.author.id)) return;
+    } else {
+        cd.add(msg.author.id);
+        setTimeout(() => {
+            cd.delete(message.author.id);
+        }, 2500);
+    }
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
     const cmd = client.commands.get(command);
