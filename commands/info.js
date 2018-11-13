6894d524314a23a8ec1d076ddc06bc8c590f6998
MrScopes <drewmanrules23@gmail.com>
2018-11-13 09:13:49 -0500

    PULL

--- commands/info.js
+++ commands/info.js
@@ -1,8 +1,8 @@
 module.exports.run = async (client, message, args) => {
     message.channel.send([
         `**CREATOR:** \`MrScopes#5546\``,
-        `**GITHUB:** https://github.com/MrScopes/Escanor`,
-        `**FORUMS:** https://escanor.ga/`,
+        `**GITHUB:** https://github.com/MrScopes/Canoe`,
+        `**FORUMS:** https://canoe.forums.gg/`,
         `**CREATED:** \`November 7th, 2018\``
     ])
 };
