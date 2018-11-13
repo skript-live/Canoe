6fb70ed7019bcf3993b66a1badf7882364393966
MrScopes <drewmanrules23@gmail.com>
2018-11-13 09:54:04 -0500

    PULL

--- commands/ping.js
+++ commands/ping.js
@@ -1,4 +1,7 @@
 module.exports.run = async (client, message, args) => {
     MSG = await message.channel.send('Fetching Ping...');
-    await MSG.edit(`While my Ping is \`${MSG.createdTimestamp - message.createdTimestamp}ms\`, my ping to the DiscordAPI is \`${Math.round(client.ping)}ms\`.`)
+    await MSG.edit([
+        `**PING:** \`${MSG.createdTimestamp - message.createdTimestamp}ms\``,
+        `**DiscordAPI:** \`${Math.round(client.ping)}ms\``
+    ])
 }
