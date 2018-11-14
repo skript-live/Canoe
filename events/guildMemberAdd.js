7529de8963f8ce0885f5d7cb929c25e7ff082f06
MrScopes <drewmanrules23@gmail.com>
2018-11-14 08:54:13 -0500

    PULL

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+module.exports = (client, message, member) => {
+    const conf = client.settings.ensure(member.guild.id, defaultS);
+    let welcome = client.settings.get(member.guild.id, 'welcome');
+    welcome = welcome.replace('{user}', member.user.tag)
+    member.guild.channels.find('name', client.settings.get(member.guild.id, 'logs')).send(welcome).catch(console.error);
+});
+    
