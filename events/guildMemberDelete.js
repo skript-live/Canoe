def870f7bf0c57470cf0224db56d4479ddcb6277
MrScopes <drewmanrules23@gmail.com>
2018-11-14 09:20:07 -0500

    PULL

--- events/guildMemberDelete.js
+++ events/guildMemberDelete.js
@@ -4,7 +4,11 @@ const client = new Discord.Client();
 module.exports = (client, member) => {
     const conf = client.settings.ensure(member.guild.id, defaultS);
     let welcome = client.settings.get(member.guild.id, 'leave');
-    leave = welcome.replace('{user}', member.user.tag)
+    leave = leave.replace('{user}', member.user.tag)
+    leave = leave.replace('{userName}', member.user.username)
+    leave = leave.replace('{userTag}', member.user.tag)
+    leave = leave.replace('{userId}', member.user.id)
+    leave = leave.replace('{userDiscriminator}', member.user.discriminator)
     member.guild.channels.find('name', client.settings.get(member.guild.id, 'logs')).send(leave).catch(console.error);
 });
     
