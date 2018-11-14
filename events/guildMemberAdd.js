6416c2a1d060576fb48fb4d5774eeb31c7539a9c
MrScopes <drewmanrules23@gmail.com>
2018-11-14 09:18:44 -0500

    PULL

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -5,6 +5,10 @@ module.exports = (client, member) => {
     const conf = client.settings.ensure(member.guild.id, defaultS);
     let welcome = client.settings.get(member.guild.id, 'welcome');
     welcome = welcome.replace('{user}', member.user.tag)
+    welcome = welcome.replace('{userName}', member.user.username)
+    welcome = welcome.replace('{userTag}', member.user.tag)
+    welcome = welcome.replace('{userId}', member.user.id)
+    welcome = welcome.replace('{userDiscriminator}', member.user.discriminator)
     member.guild.channels.find('name', client.settings.get(member.guild.id, 'logs')).send(welcome).catch(console.error);
 });
     
