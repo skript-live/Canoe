a37609c96b475010ec855b22e924544981b36ec7
MrScopes <drewmanrules23@gmail.com>
2018-11-14 23:44:52 -0500

    Delete help.js

--- commands/commands/help.js
+++
@@ -1,7 +0,0 @@
-module.exports.run = async (client, message, args) => {
-	const conf = client.settings.ensure(message.guild.id, defaultS);
-	const prefix = conf.prefix;
-	message.channel.send([
-		`View the Documentation here: https://canoe.forums.gg/documentation`
-	])
-}
