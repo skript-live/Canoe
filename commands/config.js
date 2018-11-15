9166ab5c96cd3616f320dcb5727066e15649c098
MrScopes <drewmanrules23@gmail.com>
2018-11-15 09:23:12 -0500

    pull

--- commands/config.js
+++ commands/config.js
@@ -1,81 +1,28 @@
 module.exports.run = async (client, message, args) => {
 	const conf = client.settings.ensure(message.guild.id, defaultS);
 	if (!message.member.hasPermission('MANAGE_SERVER')) {
-		let noAccess = client.settings.get(message.guild.id, 'noAccess');
-		message.channel.send(noAccess);
-		return;
+		return await message.channel.send(conf.noAccess);
 	}
 	if (!args[0] && !args[1]) {
-		message.channel.send([
+		return await message.channel.send([
 			conf.prefix,
 			conf.noAccess,
 			conf.modRole,
 			conf.welcome,
-			conf.leave,
-			conf.messageDelete
+			conf.leave
 		])
-		return;
 	}
-	if (args[0] === 'reset') {
+	if (args[0] === 'reset' && !args[1]) {
 		client.settings.delete(message.guild.id);
-		return await message.channel.send([
-			`:white_check_mark: All Configuration has been reset`,
-			`My prefix is now \`can (help)\``
-		])
+		return await message.channel.send(`Successfully reset all configuration.`)
 	}
-	if (args[0] === 'prefix') {
-		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, prefix);
-			return message.channel.send (':white_check_mark: Reset the prefix.')
-		}
-		if (args[1].length > 8) return message.channel.send('The length of the prefix must be < 8')
-		client.settings.set(message.guild.id, args[1], 'prefix');
-		message.channel.send(`:white_check_mark: Updated Configuration...`)
-		return;
+	const [prop, ...value] = args;
+	if (args[0] === 'reset' && args[1]) {
+			if(!client.settings.has(message.guild.id, prop)) return await message.channel.send('Read my documentation: https://canoe.forums.gg/docs')
+			client.settings.delete(message.guild.id, prop);
+			return await message.channel.send(`Successfully reset ${prop}.`)
 	}
-	if (args[0] === 'noAccess') {
-		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, noAccess);
-			return message.channel.send (':white_check_mark: Reset the noAccess message.')
-		}
-		return;  
-	}
-	if (args[0] === 'modRole') {
-		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, modRole);
-			return message.channel.send (':white_check_mark: Reset the modRole.')
-		}
-		return;  
-	}
-	if (args[0] === 'logs') {
-		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, prefix);
-			return message.channel.send (':white_check_mark: Reset the logs channel.')
-		}
-		return;  
-	}
-	if (args[0] === 'welcome') {
-		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, prefix);
-			return message.channel.send (':white_check_mark: Reset the welcome message.')
-		}
-		return;  
-	}
-	if (args[0] === 'leave') {
-		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, prefix);
-			return message.channel.send (':white_check_mark: Reset the farewell message.')
-		}
-		return;  
-	}
-	if (args[0] === 'messageDelete') {
-		if (args[1] === 'reset') {
-			client.settings.delete(message.guild.id, prefix);
-			return message.channel.send (':white_check_mark: Reset the messageDelete message.')
-		}
-		return;  
-	}
-	else {
-	  return message.channel.send('Read my documentation: https://canoe.forums.gg/docs')
-	}
+    if(!client.settings.has(message.guild.id, prop)) return await message.channel.send('Read my documentation: https://canoe.forums.gg/docs')
+    client.settings.set(message.guild.id, value.join(' '), prop);
+    return await message.channel.send(`Successfully updated ${prop} to:\n\`${value.join(' ')}\``)
 };
