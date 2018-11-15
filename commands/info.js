689d1e0b759a8eb9b901687c4e861f6e0ac45ce0
MrScopes <drewmanrules23@gmail.com>
2018-11-14 23:44:04 -0500

    Delete info.js

--- commands/info.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = async (client, message, args) => {
-    message.channel.send([
-        `**CREATOR:** \`MrScopes#5546\``,
-        `**GITHUB:** https://github.com/MrScopes/Canoe`,
-        `**FORUMS:** https://canoe.forums.gg/`,
-        `**CREATED:** \`November 7th, 2018\``
-    ])
-};
