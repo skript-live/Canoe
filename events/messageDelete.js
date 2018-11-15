148c12c285b614a03d5ec259479d234206c547dd
MrScopes <drewmanrules23@gmail.com>
2018-11-15 09:33:51 -0500

    pull

--- events/messageDelete.js
+++ events/messageDelete.js
@@ -1,21 +1,11 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-
 module.exports = (client, message) => {
-	if (message.author.bot) return;
-	let messageDelete = client.settings.get(message.guild.id, 'messageDelete');
-	messageDelete = messageDelete.replace('{message}', message.content)
-	.replace('{cleanMessage}', message.cleanContent)
-	.replace('{user}', message.author)
-	.replace('{userName}', message.author.authorname)
-	.replace('{userTag}', message.author.tag)
-	.replace('{userId}', message.author.id)
-	.replace('{userDiscriminator}', message.author.discriminator)
-	.replace('{channel}', message.channel)
-	.replace('{channelName}', message.channel.name)
-	.replace('{channelId}', message.channel.id)
+	if (message.author.bot || !message.guild) return;
 	log = message.guild.channels.find(c => c.name === client.settings.get(message.guild.id, 'logs'))
 	if (log) {
-		log.send(messageDelete).catch();
+		await log.send([
+			`:wastebasket: ${message.author.tag) \`(${message.author.id})\` message \`(${message.id})\` deleted in ${message.channel}:`,
+			message.cleanContent
+		])
+		.catch();
 	}
 };
